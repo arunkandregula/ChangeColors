d8d27f71c915e32ffc495ee5a7e35e383e357a10
Arun Kandregula <akandregula@kahuna.com>
2017-01-31 17:41:20 -0800

    Added more config and encapsulation

--- main.js
+++ main.js
@@ -1,21 +1,47 @@
-const randomColors = [];
-const getRandomColor = ()=>{
-    let randomColor = null;
-    do {
-        randomColor = "#" + Math.random().toString(16).substr(2,6);
-    }while(randomColors.includes(randomColor));
-    return randomColor;
-};
+class backgroundColorChanger{
 
+    // both args in seconds
+    constructor(totalTime, timePerTransition){
 
-for(let i = 0 ; i < 10; i++){
-    randomColors.push(getRandomColor());
+        this.timePerTransition = timePerTransition;
+        this.totalTime = totalTime;
+
+        // I am not adding validation checks specifically as they were not part of spec and for simplicity.
+
+        let num = totalTime/timePerTransition;
+        let colorSet = [];
+
+        this.generateRamdomColors(colorSet, num);
+
+        Object.assign(this,{
+            startChangingColors(){
+                let index  = 0;
+                let timerId = setInterval(()=>{
+                    if(index === 10){
+                        clearInterval(timerId);
+                    }
+                    document.body.style.backgroundColor = colorSet[index++];
+                }, timePerTransition * 1000);
+            }
+        });
+    }
+    generateRamdomColors(set, n){
+        // removing all elements from set and start fresh
+        set.length = 0;
+        for(let i = 0 ; i < n ; i++){
+            set.push(this.getRandomColor(set));
+        }
+        return set;
+    }
+    getRandomColor(set){
+        let randomColor = null;
+        do {
+            randomColor = "#" + Math.random().toString(16).substr(2,6);
+        }while(set.includes(randomColor));
+        return randomColor;
+    }
 }
 
-let index  = 0;
-let timerId = setInterval(()=>{
-    if(index === 10){
-        clearInterval(timerId);
-    }
-    document.body.style.backgroundColor = randomColors[index++];
-}, 1000);
+
+var bgChanger = new backgroundColorChanger(10, 1);
+bgChanger.startChangingColors();
